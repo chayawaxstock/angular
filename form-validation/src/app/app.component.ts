15e91ac08c43b8173e86114acbd648e66d99c750
chaya <chaya.waxstock@seldatinc.com>
2018-08-28 11:07:06 +0300

    hhh

--- form-validation/src/app/app.component.ts
+++ form-validation/src/app/app.component.ts
@@ -12,41 +12,5 @@ import { Person } from './shared/model/person.model';
 })
 export class AppComponent {
 
-<<<<<<< HEAD
   constructor() { }
-=======
-  
-  title = 'lands';
-
-  registerForm: FormGroup;
-
-  constructor(private formBuilder: FormBuilder,private personService:PersonService) { }
-
-  ngOnInit() {
-      this.registerForm = this.formBuilder.group({
-          name: ['',[ Validators.required,Validators.minLength(3),Validators.maxLength(15)]],
-          tz: ['', [Validators.required, Validators.minLength(9),Validators.maxLength(9),ValidateTz]],
-          country:['',[Validators.required,ValidateCountry]],
-          age:['',[Validators.required,Validators.min(0),Validators.max(120)]],
-          isMale:['',[Validators.required]]
-      });
-  }
-
-  // convenience getter for easy access to form fields
-  get f() { return this.registerForm.controls; }
-
-  onSubmit() {
-      
-      // stop here if form is invalid
-      if (this.registerForm.invalid) {
-          return;
-      }
-      let person:Person=this.registerForm.value;
-      this.personService.addPerson(person).subscribe(data=>{
-          if(data[0].status=="400")
-            return
-      });
-
-  }
->>>>>>> 56d9df9a384f2889cd7563b196497dd1270aa853
 }
--- form-validation/src/app/app.module.ts
+++ form-validation/src/app/app.module.ts
@@ -6,21 +6,15 @@ import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { PersonService } from './shared/service/person.service';
-<<<<<<< HEAD
 import { PersonListComponent } from './component/person-list/person-list.component';
 import { RegisterComponent } from './component/register/register.component';
-=======
->>>>>>> 56d9df9a384f2889cd7563b196497dd1270aa853
 //import { PersonService } from './shared/services/person.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
     PersonListComponent,
     RegisterComponent,
-=======
->>>>>>> 56d9df9a384f2889cd7563b196497dd1270aa853
  
   ],
   imports: [
